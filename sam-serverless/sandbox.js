d99349b7493a1265408c0507e6f305acdeb0ff4a
Lucidreline <casta.ma502@gmail.com>
2024-01-07 08:21:39 +0000

    Finalizing the formatted timestamp string array function.

--- sam-serverless/sandbox.js
+++ sam-serverless/sandbox.js
@@ -1,14 +1,30 @@
+const dateToFormattedString = (dateObj) => {
+    return `${("0" + dateObj.getMonth() + 1).slice(-2)}/${("0" + dateObj.getDate()).slice(-2)}/${dateObj.getFullYear()} ${("0" + dateObj.getHours()).slice(-2)}:${("0" + dateObj.getMinutes()).slice(-2)}`
+}
+
 const getXMostRecentTimestamps = (timestampQuantity) => {
     // GetBatchItem can only return 25 items at a time, for now I will cap it.
     if (timestampQuantity > 25) timestampQuantity = 25
 
-
-    const timezoneOffset = -8
+    // get the current time
     let timestamp = new Date();
-    timestamp.setHours(timestamp.getHours() - timezoneOffset)
+    timestamp.setHours(timestamp.getHours() - 8) // time zone change
 
-    const formatedTimestamp = `${("0" + timestamp.getMonth() + 1).slice(-2)}/${("0" + timestamp.getDate()).slice(-2)}/${timestamp.getFullYear()} ${("0" + timestamp.getHours()).slice(-2)}:${("0" + timestamp.getMinutes()).slice(-2)}`
-    console.log(formatedTimestamp)
+    // get the current hour at the 0 minute
+    let currentHour = new Date(timestamp.setMinutes(0))
+
+
+    // get resulting list of formatted string timestamps
+    const results = []
+    for (let i = 0; i < timestampQuantity; i++) {
+        currentHour.setHours(currentHour.getHours() - 1)
+        results.push(dateToFormattedString(currentHour))
+    }
+
+    console.log(results.reverse())
+
+
 }
 
-getXMostRecentTimestamps()
+
+getXMostRecentTimestamps(5)
