10c6c09088b893a7ea1a4fca3612624f28b084b3
Manny <casta.ma502@gmail.com>
2024-01-24 20:06:03 -0800

    Removed pages folder.

--- pages/test/page.jsx
+++
@@ -1,9 +0,0 @@
-import Header from "../components/Header/Header";
-import LocationCardList from "../components/LocationCardList/LocationCardList";
-
-export default function Page({ data }) {
-    return <div>
-        <Header />
-        <h2>Where am I?</h2>
-    </div>
-}
