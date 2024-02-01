64c32d369c58f4d165817a7792fd322b92b5d290
Manny <casta.ma502@gmail.com>
2024-01-31 23:52:32 -0800

    CardList is migrated.

--- client/src/components/LocationCardList/LocationCardList.component.jsx
+++ client/src/components/LocationCardList/LocationCardList.component.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import LocationCard from '../LocationCard/LocationCard.component'
 
 // -- Change This!!! --
-const locations = ["Outside", "Upstairs"]
+const locations = ["Outside", "Upstairs", "1", "2"]
 
 const LocationCardList = () => {
     return (
-        <div>
+        <div className='location-card-list'>
             {locations.map(location => (
                 <LocationCard location={location} />
             ))}
